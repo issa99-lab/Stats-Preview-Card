8f2da9bc582dc79855ff7f459de431ece5cbd8a4
issa99-lab <issanana99@gmail.com>
2023-05-05 19:27:46 +0300

    Challenge done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    screens: {
+      'sm': '370px',
+      'md': '690px',
+      'lg': '1024px',
+    },
+    fontFamily: {
+      inter: ['sans-serif'],
+      Montserrat: ['sans-serif'],
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
+    colors: {
+      white: '#ffffff',
+      whitish: 'hsla(0, 0%, 100%, 0.75)',
+      stats: 'hsla(0, 0%, 100%, 0.6)',
+      main: 'hsl(233, 47%, 7%)',
+      card: 'hsl(244, 38%, 16%)',
+      violet: 'hsl(277, 64%, 61%)'
+    },
   },
+
   plugins: [],
 }
 
